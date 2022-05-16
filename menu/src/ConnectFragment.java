ca88795c8b40e7e5ddcf63197a852f6a2333aa0c
DollyCup <104717248+DollyCup@users.noreply.github.com>
2022-05-16 13:00:16 +0200

    Create ConnectFragment.java

---
+++ menu/src/ConnectFragment.java
@@ -0,0 +1,16 @@
+package com.example.myapplication7;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ConnectFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_connect, container, false);
+        return rootView;
+    }
+}
