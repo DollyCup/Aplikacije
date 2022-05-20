d5bea4f736433f8fe274c2f219aae054f69859a2
DollyCup <104717248+DollyCup@users.noreply.github.com>
2022-05-20 12:37:13 +0200

    Create AppData.java

---
+++ DataBase2/src/AppData.java
@@ -0,0 +1,6 @@
+package com.example.myapplication10;
+
+public class Appdata {
+    public static  DatabaseHelper databaseHelper;
+    static Day[ ] dan =new Day[6];
+    static int lastclickedId=0;
